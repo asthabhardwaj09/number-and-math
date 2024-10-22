380f9e390bcc4de9db97aa7d5b431206ee3391e9
Astha Bhardwaj <asthabhardwaj529@gmail.com>
2024-10-22 23:58:52 +0530

    num and math

---
+++ num_and_math.js
@@ -0,0 +1,41 @@
+// const score = 400
+// // console.log(score);
+// // let a='astha'
+// // console.log(typeof a);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1))
+// console.log(typeof balance);
+
+// // let otherNumber=12.1
+// // let otherNumber=1.456
+// let otherNumber=1.14568
+// console.log(otherNumber.toPrecision(3));
+
+// const hundereds=1000000000000
+// console.log(hundereds.toLocaleString('en-IN'));
+
+//+++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(5.7));
+// console.log(Math.floor(5.7));
+// console.log(Math.min(1,2,3,4,5,6,7,8));
+// console.log(Math.max(1,2,3,4,5,6,7,8));
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const max= 20
+const min=10
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
